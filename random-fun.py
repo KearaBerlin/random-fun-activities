18d9eb34692fbcb9ac8d2d21093003f21848cfb4
Keara Berlin <kearaberlin@gmail.com>
2021-01-12 18:57:37 -0600

    refactor

--- random-fun.py
+++ random-fun.py
@@ -2,26 +2,29 @@ import random
 
 adjectives = ['Quickly', 'Seriously', 'Slowly', 'With fear,', 'Blindfolded,',
                 'Grandiosely', 'Joyfully', 'Lazily', 'Angrily']
-random_adj = random.randint(0,len(adjectives)-1)
 
 verbs = ['count', 'destroy', 'yeet', 'narrate', "don't touch", 'balance',
         'build with', 'sketch', 'photograph']
-random_verb = random.randint(0,len(verbs)-1)
 
 nouns = ['fruit', 'a chair', 'Spoons', 'spoons', 'a round object', 'paper products',
             'the ground', 'the air', 'some water', 'the first thing you see']
-random_noun = random.randint(0, len(nouns)-1)
 
 places = ['in the kitchen', 'balanced on a small surface', 'in the living room',
         'in the bedroom', 'in the basement', 'somewhere unexpected',
         'with no roof over your head', 'while horizontal', 'somewhere cold',
         'somewhere dark', 'somewhere warm', 'in the most open area']
-random_place = random.randint(0, len(places)-1)
 
-print(adjectives[random_adj], verbs[random_verb], nouns[random_noun], places[random_place])
-
-n = 1
+# Generate activity
 lists = [adjectives, verbs, nouns, places]
+activity = ''
+for list in lists:
+    random_i = random.randint(0, len(list)-1)
+    activity += list[random_i] + ' '
+
+print(activity)
+
+# Calculate total possible activities
+n = 1
 for list in lists:
     n = n*len(list)
 
